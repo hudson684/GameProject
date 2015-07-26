cc249ca41f52f9dd00e8694932005edf276bb969
hudson684 <hudson684@gmail.com>
2015-07-26 23:09:36 +1000

    Modified the Walker code to work on any axis, next is rotation to suit the axis it is working on and object avoidance

--- Assets/_Scripts/Walker.cs
+++ Assets/_Scripts/Walker.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Walker : MonoBehaviour {
 	
 	public Vector3[] patrolPoints;
+	private int currentPoint = 0;
 
 	private Transform[] hitBarrier;
 	public LayerMask toHit;
@@ -11,95 +12,56 @@ public class Walker : MonoBehaviour {
 	private bool triggered = false;
 	private bool patroling = true;
 	
-	public float Speed = 1f;
+	public float Speed = 10f;
+	private float distance = 0.1f;
+	public bool loop = false;
 
-	bool oneWayDone = false;
 
 
-
-	//set up the patrolers 2d light asset
-	void Start()
-	{
-
+	void Awake(){
+		this.transform.position = patrolPoints [0];
 	}
 
-	//if player is found, and hasn't already been triggered, stop the patroling and wait a few seconds, then shoot at player
 	void Update(){
 
-		while (!oneWayDone) {
+		Vector3 velocity = Vector3.zero;
 
+		if (currentPoint < patrolPoints.Length) {
+			Vector3 nextPoint = patrolPoints[currentPoint];
+			Vector3 moveDirection = nextPoint - this.transform.position;
 
-			for (int i = 1; i < patrolPoints.Length; i++) {
-				if (patrolPoints [i - 1].y == patrolPoints [i].y) {
-					if (patrolPoints [i - 1].x < patrolPoints [i].x) {
-						StartCoroutine (move (patrolPoints [i].x, patrolPoints [i].y, true, true));
-					} else {
-						StartCoroutine (move (patrolPoints [i].x, patrolPoints [i].y, true, false));
-					}
-				} else {
-					//more complicated code...
-				}
+			velocity = this.GetComponent<Rigidbody2D>().velocity;
+
+			if(moveDirection.magnitude < distance)
+			{
+				currentPoint++;
+			} else {
+				velocity = moveDirection.normalized * Speed;
+			} 
+		} else{
+			if(loop)
+			{
+				currentPoint = 0;
+			} else {
+				velocity = Vector3.zero;
 			}
-
-			Debug.Log(oneWayDone.ToString());
-
 		}
 
-		/*while (oneWayDone) {
+		this.GetComponent<Rigidbody2D>().velocity = velocity;
 
-			for (int i = patrolPoints.Length - 1; i > 0; i--) {
-				if (patrolPoints [i].y == patrolPoints [i - 1].y) {
-					if (patrolPoints [i].x < patrolPoints [i - 1].x) {
-						StartCoroutine (move (patrolPoints [i - 1].x, patrolPoints [i - 1].y, true, true));
-					} else {
-						StartCoroutine (move (patrolPoints [i - 1].x, patrolPoints [i - 1].y, true, false));
-					}
-				} else {
-					//more complicated code...
-				}
-			}
-		}*/
+		
+
 	}
-
-
-	/// <summary>
-	/// move the walked from any position along a fixed axis 
-	/// 	/// </summary>
-	/// <param name="xEnd">X ending location.</param>
-	/// <param name="yEnd">Y ending location.</param>
-	/// <param name="toX">If set to <c>true</c> then the walked is moveing along x, if not
-	/// the walker is moving along y.</param>
-	/// <param name="toLeftOrUp">If set to <c>true</c> then the walker is moving to either
-	/// left or up depending on the variable toX.</param>
-	IEnumerator move(float xEnd, float yEnd, bool toX, bool toLeftOrUp){
-
-		if (toX && toLeftOrUp) {
-			while(this.transform.position.x < xEnd){
-				this.transform.Translate(new Vector2(Speed * 0.01f, 0f));
-				yield return null;
-			}
-		} else if (toX && !toLeftOrUp) {
-			while(this.transform.position.x > xEnd){
-				this.transform.Translate(new Vector2(-Speed * 0.01f, 0f));
-				yield return null;
-			}
-		} else if (!toX && toLeftOrUp) {
-
-			yield return null;
-		} else if (!toX && !toLeftOrUp) {
-
-			yield return null;
-		}
-
-		oneWayDone = !oneWayDone;
-	}
-
 
 	//Development code
 	void OnDrawGizmos(){
 
 		for (int i = 1; i < patrolPoints.Length; i++) {
 			Gizmos.DrawLine(patrolPoints[i-1],patrolPoints[i]);
+		}
+
+		if (loop) {
+			Gizmos.DrawLine(patrolPoints[patrolPoints.Length - 1], patrolPoints[0]);
 		}
 
 	}
