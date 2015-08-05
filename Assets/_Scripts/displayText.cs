d331e20cd754c93f3b0ae3bfa59bed38692930c7
Minimaximize <liampatrickjames@gmail.com>
2015-08-05 10:50:37 +1000

    Post Crit commit

---
+++ Assets/_Scripts/displayText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displayText : MonoBehaviour {
+
+	private Canvas Display;
+
+	// Use this for initialization
+	void Start () {
+		Display = GetComponentInChildren<Canvas>();
+		Display.enabled =false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			Display.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "Player"){
+			Display.enabled = false;
+		}
+	}
+}
