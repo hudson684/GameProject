f2b2d24e19b8e67afcbac661baccb520d4358a51
Minimaximize <liampatrickjames@gmail.com>
2015-08-10 10:35:28 +1000

    Indicator lights now appear above  doors, Indicator lights generated based on the number of keys on a lock.

---
+++ Assets/_Scripts/Indicator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Indicator : MonoBehaviour {
+
+	public Key refkey;
+	public Material red,green;
+	private Renderer indComponent;
+
+	// Use this for initialization
+	void Start () {
+		indComponent = gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//assess if instance has a reference key
+		if(refkey != null){//if reference key is set check if locked
+			if(!refkey.getLocked()){
+				indComponent.material = green;
+			}else{
+				indComponent.material = red;
+			}
+		}else{
+			//if reference key not set, default to green
+			indComponent.material = green;
+		}
+	}
+
+	public void setKey(Key key){
+		refkey = key;
+	}
+}
--- Assets/_Scripts/Lock.cs
+++ Assets/_Scripts/Lock.cs
@@ -1,17 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Lock
+/// 
+/// Assesses the state of a door based on the states of associated keys.
+/// While one or more keys are locked, the door remains locked
+/// 
+/// If no keys assigned to a lock, lock is automatically open.
+/// </summary>
 public class Lock : MonoBehaviour {
 
+	//All keys associated to lock
 	public Key[] Keys;
 	public bool locked = true;
 
+	//Setup for indicator Lights
+	public GameObject indicator;
+	public Vector3 padding;
+	private Vector3 origin;
+
 	void Start(){
-		if(Keys.Length == 0){
+		//Set origin for indicator lights
+		origin = gameObject.transform.FindChild("IndicatorOrigin").transform.position;
+		if(Keys.Length == 0){//if no keys, door is automatically unlocked
 			locked = false;
+			Instantiate(indicator, origin, Quaternion.identity);
+		}else{//if keys greater than zero, instantiate an indicator for each key
+			for(int i = 0; i < Keys.Length; i++){
+				//create an indicator and set its key
+				GameObject anIndicator = Instantiate(indicator, origin + (padding * i), Quaternion.identity) as GameObject;
+				anIndicator.GetComponent<Indicator>().setKey(Keys[i]);
+			}
 		}
 	}
-	
+
+	/// <summary>
+	/// Raises the collision stay2 d event.
+	/// 
+	/// If the player collides with the door. Door assesses key for state
+	/// </summary>
+	/// <param name="other">Player</param>
 	void OnCollisionStay2D(Collision2D other){
 		if(other.transform.tag == "Player"){
 
@@ -26,15 +55,26 @@ public class Lock : MonoBehaviour {
 		}
 	}//end OnCollisonEnter2D
 
+	/// <summary>
+	/// Raises the trigger exit2 d event.
+	/// 
+	/// Makes the doors collider solid
+	/// </summary>
+	/// <param name="other">Player</param>
 	void OnTriggerExit2D(Collider2D other){
 		if(other.tag == "Player"){
 			close ();
 		}
 	}
 	
-
+	/// <summary>
+	/// Checks if all keys are set to locked;
+	/// </summary>
+	/// <returns><c>true</c> one or more keys are still locked
+	/// <c>false</c> all keys are ulocked</returns>
 	bool checkKeys(){
 		for(int i = 0; i< Keys.Length; i++){
+			//'And'gate, only assesses as false if all keys are false
 			if(Keys[i].getLocked()){
 				return true;
 			}
@@ -42,14 +82,25 @@ public class Lock : MonoBehaviour {
 		return false;
 	}
 
+	/// <summary>
+	/// Sets the collider to trigger, allowing the player to pass
+	/// </summary>
 	void open(){
 			gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
 	}
 
+	/// <summary>
+	/// Sets the collider to collider, making the object solid
+	/// </summary>
 	void close(){
 		gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
 	}
 
+	/// <summary>
+	/// Raises the draw gizmos event.
+	/// 
+	/// draws a line to all associated keys
+	/// </summary>
 	void OnDrawGizmos(){
 		for(int i = 0; i < Keys.Length; i++){
 			if(Keys[i] != null){
