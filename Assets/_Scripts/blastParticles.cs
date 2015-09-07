d58f7eabce919df958972f05e0c6bba0d0c0204e
Minimaximize <liampatrickjames@gmail.com>
2015-09-07 13:23:35 +1000

    Pre Test Changes, All Levels now have particles +more readabilty!
    Laser sounds now trail off as you move away. Lasers now collide with the right layers

--- Assets/_Scripts/blastParticles.cs
+++ Assets/_Scripts/blastParticles.cs
@@ -4,10 +4,17 @@ using System.Collections;
 public class blastParticles : MonoBehaviour {
 
 	public ParticleSystem particles;
+	private Lock parentLock;
+
+	void Start(){
+		parentLock = GetComponentInParent<Lock>();
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if(other.tag == "Player"){
-			particles.Play();
+		if(!parentLock.checkKeys()){
+			if(other.tag == "Player"){
+				particles.Play();
+			}
 		}
 	}
 }
