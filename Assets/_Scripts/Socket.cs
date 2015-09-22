53c1fcb9757557ee31f0ed8c53647332e7916592
Minimaximize <liampatrickjames@gmail.com>
2015-09-22 22:48:14 +1000

    Sockets and Cores Improved

--- Assets/_Scripts/Socket.cs
+++ Assets/_Scripts/Socket.cs
@@ -3,11 +3,34 @@ using System.Collections;
 
 public class Socket : Key {
 
+	//target
 	public Collider2D refCore;
+
+	//Public Positioning
+	public float transitionTime = 1.5f;
+	public float transitionSpeed = 5f;
+
+	//Positioning
+	private float transitionTemp;
 	private Transform refTransform;
+	private bool rotate = false;
+	private bool move = false;
+
+	//Animation
+	private Animator anim;
+	private int closedHash = Animator.StringToHash("Close");
+
 
 	void Start(){
 		refTransform = transform.FindChild("DummyCore");
+		transitionTemp = transitionTime;
+		anim = transform.FindChild("Reactor Socket").GetComponent<Animator>();
+	}
+
+	void Update(){
+		rotateToPosition();
+		moveToPosition();
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
@@ -15,12 +38,38 @@ public class Socket : Key {
 			Unlock();
 			refCore.GetComponent<Rigidbody2D>().Sleep();
 			refCore.GetComponent<BoxCollider2D>().enabled = false;
-			refCore.transform.position = refTransform.position;
-			refCore.transform.rotation = refTransform.rotation;
-			if(refCore.transform.parent.tag == "Player"){
-				refCore.transform.SetParent(null);
-			}
+			rotate = true;
+			//refCore.transform.rotation = refTransform.rotation;
+			//refCore.transform.position = refTransform.position;
 			refCore.transform.SetParent(transform);
+		}
+	}
+
+	void rotateToPosition(){
+		if(rotate){
+			if(transitionTemp > 0){
+				refCore.transform.rotation = Quaternion.Slerp(refCore.transform.rotation,refTransform.rotation, Time.deltaTime * transitionSpeed);
+				transitionTemp -= Time.deltaTime;
+			}else{
+				refCore.transform.rotation = refTransform.rotation;
+				rotate = false;
+				move = true;
+				transitionTemp = transitionTime;
+			}
+		}
+	}
+
+	void moveToPosition(){
+		if(move){
+			if(transitionTemp > 0){
+				refCore.transform.position = Vector3.Slerp(refCore.transform.position,refTransform.position, Time.deltaTime * transitionSpeed);
+				transitionTemp -= Time.deltaTime;
+			}else{
+				refCore.transform.position = refTransform.position;
+				move = false;
+				transitionTemp = transitionTime;
+				anim.SetBool(closedHash,true);
+			}
 		}
 	}
 
