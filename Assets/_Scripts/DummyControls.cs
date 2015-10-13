2c80e4bfdbed1ab9b18996d94208824d2d72e161
Minimaximize <liampatrickjames@gmail.com>
2015-10-14 02:00:37 +1000

    Final Commit for the Night
    Fingers Crossed The animations Work

---
+++ Assets/_Scripts/DummyControls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyControls : MonoBehaviour {
+
+	private Animator anim;
+	private int upHash = Animator.StringToHash("getUp");
+	public Canvas titleCard;
+
+	private bool startTimer = false;
+	public float timeR = 3;
+
+	void Start(){
+		anim = gameObject.GetComponent<Animator>();
+		titleCard.enabled = false;
+	}
+
+	void Update(){
+		if(Input.anyKey){
+			anim.SetBool(upHash, true);
+		}
+		if(startTimer){
+			if(timeR > 0 ){
+				countDown();
+			}else{
+				Application.LoadLevel(Application.loadedLevel+1);
+			}
+		}
+	}
+
+	void openCanvas(){
+		titleCard.enabled = true;
+		startTimer = true;
+	}
+
+	void countDown(){
+		timeR -= Time.deltaTime;
+	}
+}
---
+++ Assets/_Scripts/IntroSequenceCut.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroSequenceCut : MonoBehaviour {
+
+	public GameObject[] GLrigs;
+	public DummyControls dummy;
+
+
+	// Use this for initialization
+	void Start () {
+		foreach(GameObject rig in GLrigs){
+			rig.SetActive(false);
+		}
+	}
+	
+	public void clearRigs(){
+		foreach(GameObject rig in GLrigs){
+			rig.SetActive(false);
+		}
+	}
+
+	public void useRig(int i){
+		clearRigs();
+		GLrigs[i].SetActive(true);
+	}
+
+	public void enableDummy(){
+		dummy.enabled = true;
+	}
+}
